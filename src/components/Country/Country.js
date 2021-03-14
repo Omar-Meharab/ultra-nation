e7a44cb87d2749dae7e2fb5eadb8c9d858b6f176
Omar Meharab <omarmeharab81@gmail.com>
2021-03-14 11:39:28 +0600

    a

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Country = (props) => {
+    console.log(props.country.name);
     const {name, population, region, flag} = props.country;
     const flagStyle = {height: '50px'}
     const countryStyle = {border: '1px solid red', margin: '10px', padding: '10px'}
